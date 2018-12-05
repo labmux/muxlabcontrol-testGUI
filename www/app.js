393d98ad3415364dd5bc46d97691fbbdacee6c6c
Eliran Assaraf <eliran.a.bg@gmail.com>
2018-12-05 11:15:56 -0500

    first commit

---
+++ www/app.js
@@ -0,0 +1,29 @@
+angular.module('uiBootstrap', ['ui.router', 'ui.bootstrap']);
+angular.module('uiBootsrtap').controller('ModalCtrl', function ($uibModal) {
+    var $ctrl = this;
+
+    $ctrl.animationsEnabled = true;
+
+    $ctrl.mnc.name;
+    $ctrl.mnc.version = ['1.0.0','1.0.1','1.1.1','2.0.5'];
+    $ctrl.app.version = ['1.0.0','1.0.1','1.1.1','2.0.5'];
+
+    $ctrl.addMnc = function () {
+        var modalInstance = $uibModal.open({
+            animation: $ctrl.animationsEnabled,
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: 'myModalContent.html',
+            controller: 'ModalInstanceCtrl',
+            controllerAs: '$ctrl',
+            size: size,
+            appendTo: parentElem,
+            resolve: {
+                version: function () {
+                    return $ctrl.mnc.version;
+                }
+            }
+        });
+
+    }
+})
